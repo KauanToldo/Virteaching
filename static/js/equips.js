a2b72c9e6485515fbc2b8fc69968515961617a28
WilliamKunzler <wkwilliamkunzler@gmail.com>
2024-08-24 17:02:44 -0300

    fim

--- static/js/equips.js
+++ static/js/equips.js
@@ -1,16 +1,16 @@
 
 
-function changeEquips(){
 
+function changeEquips() {
     
+    cards.forEach();
 
 }
-
 
 var left_chevron = document.querySelector("#left-chevron").addEventListener('click', changeEquips);
 var right_chevron = document.querySelector("#right-chevron").addEventListener('click', changeEquips);
 
-var cards = document.querySelector(".cards");
+var cards = document.querySelectorAll(".cards");
 
 
 
