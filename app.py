9a5eb75194d7eba29b6cc5ced36fcde3e5a6e689
Kauan <kauanlucastoldo@gmail.com>
2024-08-24 08:58:39 -0300

    css treinamento

--- app.py
+++ app.py
@@ -5,23 +5,28 @@ app = Flask(__name__)
 
 areas_dict = {
     "Construção Civil": {
-        "image": "construcao_civil.png",
+        "id": "contrucao_civil",
+        "image": "../static/assets/construcao_civil.png",
         "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     "Engenharia de Reatores": {
-        "image": "engenharia_reatores.png",
+        "id": "reatores",
+        "image": "../static/assets/engenharia_reatores.png",
         "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     "Indústria de Simuladores de voo": {
-        "image": "simulador_voo.png",
+        "id": "voo",
+        "image": "../static/assets/simulador_voo.png",
         "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     "Medicina": {
-        "image": "medicina.png",
+        "id": "medicina",
+        "image": "../static/assets/medicina.png",
         "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     "Odontologia": {
-        "image": "odontologia.png",
+        "id": "odontologia",
+        "image": "../static/assets/odontologia.png",
         "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     }
 }
