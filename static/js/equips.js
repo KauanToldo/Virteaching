6aa55282af37ec17940a8a7c20aaa6d487fc3dee
WilliamKunzler <wkwilliamkunzler@gmail.com>
2024-08-24 19:34:55 -0300

    cards

--- static/js/equips.js
+++ static/js/equips.js
@@ -1,14 +1,43 @@
 
+let condition_plus = 0;
 
+function changeEquipsRight() {
 
-function changeEquips() {
+    condition_plus++;
+    condition_plus = condition_plus % 10;
+
     
-    cards.forEach();
+    cards.forEach(e => {
+        if (e.id == condition_plus){
+            e.classList.remove("active");
+            e.classList.add("none");
+        }
+    });
 
+
+    let nextCondition = (condition_plus + 3);
+
+
+    cards.forEach(e => {
+
+        if (nextCondition > 9){nextCondition = condition_plus - 6}
+        console.log(nextCondition);
+
+        if (e.id == nextCondition) {
+            e.classList.remove("none");
+            e.classList.add("active");
+        }
+    });
 }
 
-var left_chevron = document.querySelector("#left-chevron").addEventListener('click', changeEquips);
-var right_chevron = document.querySelector("#right-chevron").addEventListener('click', changeEquips);
+function changeEquipsLeft() {
+    
+}
+
+
+
+var left_chevron = document.querySelector("#left-chevron").addEventListener('click', changeEquipsLeft);
+var right_chevron = document.querySelector("#right-chevron").addEventListener('click', changeEquipsRight);
 
 var cards = document.querySelectorAll(".cards");
 
