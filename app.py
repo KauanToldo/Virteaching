3ab3e9deeb859e1d041767dd43b40f2c2e21ea15
Yasmin Maria Zerbielli <113360477+yasminzerbielli@users.noreply.github.com>
2024-09-30 12:36:34 -0300

    textos

--- app.py
+++ app.py
@@ -7,27 +7,27 @@ areas_dict = {
     "Construção Civil": {
         "id": "contrucao_civil",
         "image": "../static/assets/construcao_civil.png",
-        "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        "conteudo": "Na construção civil, a realidade virtual (RV) está sendo utilizada como uma ferramenta complementar de ensino, especialmente atraente para jovens inseridos no campo da informática. Um exemplo é o ambiente protótipo desenvolvido por Stange et al. (2012), que permite simular o treinamento em um canteiro de obras virtual. Os aprendizes podem praticar tarefas como a montagem de formas para pilares de concreto. Resultados de testes com RV mostraram uma melhoria significativa na identificação de riscos e na aplicação de medidas preventivas em comparação ao método de ensino tradicional, destacando a eficácia dessa tecnologia na formação profissional."
     },
     "Engenharia de Reatores": {
         "id": "reatores",
         "image": "../static/assets/engenharia_reatores.png",
-        "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        "conteudo": "Na área de engenharia de reatores, o aprendizado teórico enfrentava desafios devido à complexidade da disciplina, comprometendo a qualidade do ensino. Para resolver esse problema, foi desenvolvido um ambiente virtual utilizando tecnologias de realidade virtual para auxiliar docentes no ensino dos componentes principais de engenharia de reatores. O software SolidWorks foi utilizado para modelagem tridimensional, e a criação do ambiente virtual foi realizada com o CMS WordPress, incorporando a realidade virtual interativa por meio da ferramenta A360 Viewer da AutoDesk. Essa abordagem visa melhorar a compreensão e a aplicação prática dos conceitos."
     },
     "Indústria de Simuladores de voo": {
         "id": "voo",
         "image": "../static/assets/simulador_voo.jpg",
-        "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        "conteudo": "A realidade virtual (RV) originou-se na indústria dos simuladores de voo, que se expandiu após a Segunda Guerra Mundial com o investimento da Força Aérea Americana. O Sensorama, um dos primeiros protótipos, permitia que pilotos-alunos se familiarizassem com a cabine da aeronave, oferecendo uma experiência imersiva com visão tridimensional, aromas e vibrações. Em 1965, Ivan Sutherland desenvolveu o projeto The Ultimate Display, que permitia ao usuário explorar ambientes virtuais movendo a cabeça. Apesar das inovações na aviação, a área de simuladores de voo ainda apresenta um grande potencial a ser explorado."
     },
     "Medicina": {
         "id": "medicina",
         "image": "../static/assets/medicina.png",
-        "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        "conteudo": "As aplicações da realidade virtual (RV) estão se expandindo para diversas áreas, incluindo o treinamento de futuros médicos e enfermeiros, que enfrentam grandes desafios devido à complexidade dos procedimentos e à necessidade de interação com seres humanos. Tradicionalmente, o ensino de anatomia, fundamental para a formação desses profissionais, é realizado em laboratórios que utilizam corpos humanos e animais, modelos anatômicos, vídeos, imagens e multimídia. No entanto, esse método apresenta diversos desafios, especialmente na percepção do realismo pelos estudantes. Com o objetivo de solucionar esse problema, foi criado o projeto Virtual Interactive Distance-learning on Anatomy (VIDA), onde foram desenvolvidas interfaces interativas tridimensionais imersivas."
     },
     "Odontologia": {
         "id": "odontologia",
         "image": "../static/assets/odontologia.jpg",
-        "conteudo": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        "conteudo": "Com base nos resultados obtidos do projeto VIDA e de iniciativas semelhantes, foi desenvolvido o VIDA Odonto, um protótipo de um ambiente virtual imersivo. Esse módulo utiliza interação tridimensional para o treinamento em anestesia odontológica, proporcionando maior eficácia e realismo ao aprendizado. Além disso, oferece uma avaliação automática para a precisão dos procedimentos realizados pelo estudante, permitindo a gravação para futura avaliação (TORI et al., 2018)."
     }
 }
 
